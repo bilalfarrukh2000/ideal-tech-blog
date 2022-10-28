55d7a843da51186a86c237bfac78792801722940
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-10-28 19:51:57 -0400

    Deploy again

--- config/connection.js
+++ config/connection.js
@@ -5,25 +5,9 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DB_LINK) {
-  sequelize = new Sequelize(process.env.DB_LINK, {dialectOptions: {
-    ssl: {
-        rejectUnauthorized: false
-    }
-}});
-} else {
-  console.log('Using local DB');
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PWD,    
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    },
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0',
-  );
-}
+
+  sequelize = new Sequelize(process.env.DB_LINK,  null, null, {})
+
+
 
 module.exports = sequelize;
